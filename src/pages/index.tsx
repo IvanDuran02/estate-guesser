faa5458790cb3a788132a18dcdffe9a83176c288
Ivan Duran <iamivanduran02@gmail.com>
2022-08-22 14:17:15 -0400

    added sign in & sign out to toolbar, toolbar is also collapsible now and toolbar UI has been close to finished.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,9 @@ import {
   ChevronLeftIcon,
   ChevronRightIcon,
   CogIcon,
+  LogoutIcon,
   MenuIcon,
+  XIcon,
 } from "@heroicons/react/solid";
 import { useSession, signIn, signOut } from "next-auth/react";
 import github from "../assets/github.png";
@@ -23,22 +25,14 @@ const Home: NextPage = () => {
   // const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const { session }: any = useSession();
+  const { data: session }: any = useSession();
 
   const [menuToggle, setMenuToggle] = useState(false);
 
   const menuOpen = () => {
     setMenuToggle(!menuToggle);
+    console.log("menu toggled");
   };
-
-  if (session) {
-    return (
-      <div className="text-white">
-        <div>You are logged in</div>
-        <button onClick={() => signOut()}>Sign Out!</button>
-      </div>
-    );
-  }
 
   return (
     <>
@@ -48,21 +42,62 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex md:justify-between justify-center flex-row-reverse items-center h-screen w-screen font-mono bg-[#202020] text-white p-12 relative text-center overflow-hidden">
-        <div className="bg-black opacity-60 h-[100vh] -m-12  md:flex justify-between p-2 items-center flex-col rounded-lg w-[4%] hover:opacity-80 transition-all hidden">
-          <MenuIcon
-            className="w-6 lg:w-8  mt-4 hover:opacity-100 cursor-pointer hover:text-red-400"
-            onClick={() => menuOpen()}
-          />
+      <main className="flex md:justify-between justify-center flex-row-reverse items-center h-screen w-screen font-mono bg-[#202020] text-white p-12 relative text-center overflow-hidden ">
+        {!menuToggle ? (
+          <div className="bg-black opacity-60 h-[100vh] -m-12  md:flex justify-between p-2 items-center flex-col rounded-lg w-[4%] hover:opacity-80 transition-all hidden">
+            <MenuIcon
+              className="w-6 lg:w-8  mt-6 hover:opacity-100 cursor-pointer hover:text-red-400"
+              onClick={() => menuOpen()}
+            />
+            <div>
+              <LogoutIcon
+                className="w-6 lg:w-8 hover:text-red-400 text-white"
+                onClick={() => signOut()}
+              />
+              <br />
+              <CogIcon className="w-6 lg:w-8 hover:animate-spin hover:text-red-400 transition-all mb-6" />
+            </div>
+          </div>
+        ) : (
+          <>
+            <div className="bg-black opacity-60 h-[100vh] -m-12  md:flex flex-row-reverse p-2 items-center rounded-lg w-[14%] hover:opacity-80 transition-all hidden z-10">
+              <div className="flex flex-col justify-between h-screen items-center p-2">
+                <XIcon
+                  className="w-6 lg:w-8  mt-6 hover:opacity-100 cursor-pointer hover:text-red-400"
+                  onClick={() => menuOpen()}
+                />
 
-          <CogIcon className="w-6 lg:w-8 hover:animate-spin hover:text-red-400 transition-all mb-4" />
-        </div>
+                <CogIcon className="w-6 lg:w-8 hover:animate-spin hover:text-red-400 transition-all mb-6" />
+              </div>
+              <div className="h-[91vh] bg-red-400 w-1 rounded-full" />
+              <div className="flex justify-center items-center h-screen w-[100%]">
+                {!session ? (
+                  <div className="hover:text-red-400 hover:scale-105 transition-all">
+                    <button onClick={() => signIn()}>Sign In</button>
+                  </div>
+                ) : (
+                  <div>
+                    <button
+                      onClick={() => signOut()}
+                      className="hover:text-red-400 hover:scale-105 transition-all"
+                    >
+                      Sign Out!
+                    </button>
+                  </div>
+                )}
+              </div>
+            </div>
+          </>
+        )}
+
         <div className="flex flex-col w-screen items-center">
           <div>
             <h1 className="text-4xl font-bold text-center">Estate Guesser</h1>
-            <p className="opacity-50 text-center">
-              Guess the house price to win!
-            </p>
+            {!session ? (
+              <p className="opacity-50 text-center">Login to save progress!</p>
+            ) : (
+              <p className="opacity-50 text-center">session.user.name</p>
+            )}
           </div>
 
           <div className="flex justify-center items-center my-12">
@@ -76,7 +111,7 @@ const Home: NextPage = () => {
                 className="rounded-md shadow-lg"
               />
             </div>
-            <ChevronRightIcon className="h-16 w-16 hover:scale-110 hover:text-red-400 cursor-pointer transition-all pl-2 hover:translate-x-1" />
+            <ChevronRightIcon className="h-16 w-16 hover:scale-110 hover:text-red-400 cursor-pointer transition-all pl-2 hover:translate-x-1 z-0" />
           </div>
           <div>
             <input

a3c155cbed3cc7e9430adbd291bfcb95f9f389d6
Ivan Duran <iamivanduran02@gmail.com>
2022-08-22 13:27:27 -0400

    changed around some of the styling containers and added a menu bar to the right of the page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,16 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import exampleHouse from "../assets/house.jpeg";
-import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/solid";
+import {
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  CogIcon,
+  MenuIcon,
+} from "@heroicons/react/solid";
 import { useSession, signIn, signOut } from "next-auth/react";
 import github from "../assets/github.png";
 import Link from "next/link";
+import { useState } from "react";
 // import { trpc } from "../utils/trpc";
 
 // type Card = {
@@ -17,7 +23,13 @@ const Home: NextPage = () => {
   // const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
 
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const { data: session }: any = useSession();
+  const { session }: any = useSession();
+
+  const [menuToggle, setMenuToggle] = useState(false);
+
+  const menuOpen = () => {
+    setMenuToggle(!menuToggle);
+  };
 
   if (session) {
     return (
@@ -36,52 +48,45 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center h-screen w-screen font-mono bg-[#202020] text-white p-12 justify-between relative text-center">
-        <div>
-          <div className="flex justify-between items-center w-auto">
-            <div className="w-8 h-8 opacity-50 hover:opacity-100 cursor-pointer absolute top-14 left-96">
-              <Link
-                href="https://github.com/IvanDuran02/estate-guesser"
-                target={"_blank"}
-              >
-                <Image src={github} alt="github" />
-              </Link>
-            </div>
+      <main className="flex justify-between flex-row-reverse items-center h-screen w-screen font-mono bg-[#202020] text-white p-12 relative text-center overflow-hidden">
+        <div className="bg-black opacity-60 h-[100vh] -m-12  flex justify-between p-2 items-center flex-col rounded-lg w-[4%] hover:opacity-80 transition-all ">
+          <MenuIcon
+            className="w-8 h-8 mt-4 hover:opacity-100 cursor-pointer hover:text-red-400"
+            onClick={() => menuOpen()}
+          />
 
-            <h1 className="text-4xl text-center">Estate Guesser</h1>
-
-            <button
-              onClick={() => signIn()}
-              className="opacity-50 hover:opacity-100 hover:scale-105 transition-all absolute top-14 right-96"
-            >
-              Sign In
-            </button>
-          </div>
-          <p className="opacity-60 text-center">
-            Guess the house price to win!
-          </p>
+          <CogIcon className="w-8 h-8 hover:animate-spin hover:text-red-400 transition-all mb-4" />
         </div>
-        <div className="flex justify-center items-center">
-          <ChevronLeftIcon className="h-16 w-16 hover:scale-110 hover:text-red-400 cursor-pointer transition-all pr-2 hover:-translate-x-1" />
+        <div className="flex flex-col w-screen">
           <div>
-            <Image
-              src={exampleHouse}
-              alt="House"
-              width={900}
-              height={508}
-              className="rounded-md shadow-lg"
+            <h1 className="text-4xl font-bold text-center">Estate Guesser</h1>
+            <p className="opacity-50 text-center">
+              Guess the house price to win!
+            </p>
+          </div>
+
+          <div className="flex justify-center items-center my-12">
+            <ChevronLeftIcon className="h-16 w-16 hover:scale-110 hover:text-red-400 cursor-pointer transition-all pr-2 hover:-translate-x-1" />
+            <div>
+              <Image
+                src={exampleHouse}
+                alt="House"
+                width={900}
+                height={508}
+                className="rounded-md shadow-lg"
+              />
+            </div>
+            <ChevronRightIcon className="h-16 w-16 hover:scale-110 hover:text-red-400 cursor-pointer transition-all pl-2 hover:translate-x-1" />
+          </div>
+          <div>
+            <input
+              type=""
+              placeholder="Guess Price!"
+              className="w-[40vh] items-center text-center h-8 rounded-xl outline-none text-white bg-[black] shadow-xl "
             />
           </div>
-          <ChevronRightIcon className="h-16 w-16 hover:scale-110 hover:text-red-400 cursor-pointer transition-all pl-2 hover:translate-x-1" />
+          <p className="opacity-60">Note: App still in production!</p>
         </div>
-        <div>
-          <input
-            type=""
-            placeholder="Guess Price!"
-            className="w-[40vh] items-center text-center h-8 rounded-xl outline-none text-white bg-[black] shadow-xl "
-          />
-        </div>
-        <p className="opacity-60">Note: App still in production!</p>
       </main>
     </>
   );
